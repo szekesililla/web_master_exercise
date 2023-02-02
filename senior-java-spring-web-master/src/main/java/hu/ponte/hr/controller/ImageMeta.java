822155cae42f4133aa5d598a98886691608e38ac
szekesililla <szekesililla93@gmail.com>
2023-02-02 20:16:46 +0100

    initial commit

---
+++ senior-java-spring-web-master/src/main/java/hu/ponte/hr/controller/ImageMeta.java
@@ -0,0 +1,19 @@
+package hu.ponte.hr.controller;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author zoltan
+ */
+@Getter
+@Builder
+public class ImageMeta
+{
+	private String id;
+	private String name;
+	private String mimeType;
+	private long size;
+	private String digitalSign;
+	//private byte[] photo;
+}
